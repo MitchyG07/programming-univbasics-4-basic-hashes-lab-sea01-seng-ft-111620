41f638a00133bd0d4a28a55aa6897239cd6bdf38
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-09 20:34:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,10 @@ def my_hash
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {
+    name: "Grace Hopper"
+  }
+  return pioneer[name]
 end
 
 
